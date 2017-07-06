a122bc3745ee4768d2d2215a859fdb2555b1fae7
Parshant <parshant@ucreate.co.in>
2017-07-06 13:14:03 +0530

    test commit to staging

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel test
+                   stagging test
                 </div>
 
                 <div class="links">
