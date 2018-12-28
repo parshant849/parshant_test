30ec58f356ade7f8610ce9ab4c96dacd40e50fbc
Parshant <parshant@ucreate.co.in>
2018-12-28 16:01:03 +0530

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   stagging test 123
+                   stagging test 12345
                 </div>
 
                 <div class="links">
