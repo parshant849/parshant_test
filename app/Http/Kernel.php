3ff015d3f31f766f0b40d555c8bed1e6961dc6ba
Parshant <parshant@ucreate.co.in>
2019-01-29 15:10:43 +0530

    test update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,9 +19,10 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+
     ];
 
-    /**
+    /**ljskdlfj kljasdfk ljaskdflj lasjfkdlj kasdfj
      * The application's route middleware groups.
      *
      * @var array
