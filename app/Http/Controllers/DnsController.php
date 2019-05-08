1b2aa7a29bc1604ff58d7330dd77e19a5c883822
parshant849 <parshant849@gmail.com>
2019-05-08 13:22:56 +0530

    Update DnsController.php

--- app/Http/Controllers/DnsController.php
+++ app/Http/Controllers/DnsController.php
@@ -3,16 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Spatie\Dns\Dns;
 
 class DnsController extends Controller
 {
     public function getSiteDnsInfo(Request $request) {
 	$input = $request->all();
 	if(isset($input['site'])) {
-		$dns = new Dns($input['site']);
 		
-		$r = $dns->getRecords(['TXT']);
+
+		$r = dns_get_record($input['site']);
+
 		echo '<pre>'; print_r($r); die;	
 	} else {
 		echo 'sdfs'; die;	
